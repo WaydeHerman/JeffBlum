52989f34bf00caa9811baa31a880b4e49daa279a
Wayde Herman <wayde.h@gmail.com>
2019-10-03 18:16:21 +0200

    update viz

--- get_data.py
+++ get_data.py
@@ -1,12 +1,13 @@
 from fredapi import Fred
 import pandas as pd
 
-fred = Fred(api_key='c2c4766ab4720a73ce1c3f0bb2e21911')
-
 # more on this here:
 # https://github.com/mortada/fredapi
 
-# series IDs & start dates:
+# Set API Key here:
+fred = Fred(api_key='c2c4766ab4720a73ce1c3f0bb2e21911')
+
+# Series IDs & start dates:
 # 1M UST - DGS1MO - 2001-07-31
 # 3M UST - DGS3MO - 1982-01-04
 # 6M UST - DGS6MO - 1982-01-04
@@ -19,6 +20,8 @@ fred = Fred(api_key='c2c4766ab4720a73ce1c3f0bb2e21911')
 # 20Y UST - DGS20 - 1993-10-01
 # 30Y UST - DGS30 - 1977-02-15
 
+
+# Use API to retreive data from earliest date with all values:
 UST1M = fred.get_series('DGS1MO', observation_start='2001-07-31')
 UST3M = fred.get_series('DGS3MO', observation_start='2001-07-31')
 UST6M = fred.get_series('DGS6MO', observation_start='2001-07-31')
@@ -31,6 +34,7 @@ UST10Y = fred.get_series('DGS10', observation_start='2001-07-31')
 UST20Y = fred.get_series('DGS20', observation_start='2001-07-31')
 UST30Y = fred.get_series('DGS30', observation_start='2001-07-31')
 
+# Create dataframe with all values:
 data = pd.DataFrame()
 data['Date'] = UST1M.index
 data['T1M'] = UST1M.values
@@ -45,5 +49,12 @@ data['T10Y'] = UST10Y.values
 data['T20Y'] = UST20Y.values
 data['T30Y'] = UST30Y.values
 
+# Remove date entries with missing values:
+for index, row in data.iterrows():
+    if pd.isna(row['T1M']) == True and pd.isna(row['T3Y']) == True:
+        data.drop(index, inplace=True)
+
 data.set_index('Date', inplace=True)
+
+# Export to CSV file:
 data.to_csv('api_data.csv')
